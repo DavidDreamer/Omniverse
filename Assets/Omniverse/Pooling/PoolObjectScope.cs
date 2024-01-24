d861d419f0b0be5d9a027549dc392867d181a929
DavidDreamer <davidb.gamedev@gmail.com>
2024-01-24 03:52:25 +0300

    prefab object scope added

---
+++ Assets/Omniverse/Pooling/PoolObjectScope.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+namespace Omniverse
+{
+	public readonly struct PoolObjectScope<T>: IDisposable where T: MonoBehaviour, IPoolObject
+	{
+		private PrefabPool<T> Pool { get; }
+	
+		private T Instance { get; }
+	
+		public PoolObjectScope(PrefabPool<T> pool, T prefab)
+		{
+			Pool = pool;
+			Instance = pool.Take(prefab);
+		}
+
+		public void Dispose()
+		{
+			if (Instance == null)
+			{
+				return;
+			}
+		
+			Pool.Return(Instance);
+		}
+	}
+}
