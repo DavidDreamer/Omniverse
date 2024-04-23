0729d8436ba48ebff5b18b217b7e4ecec407d883
DavidDreamer <davidb.gamedev@gmail.com>
2024-04-23 09:17:05 +0300

    unit renderer added

---
+++ Assets/Omniverse/Scripts/Core/Units/UnitRenderer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Omniverse
+{
+	public class UnitRenderer: MonoBehaviour
+	{
+		private static class AnimatorVariables
+		{
+			public static int IsMoving { get; } = Animator.StringToHash(nameof(IsMoving));
+			public static int MovementSpeed { get; } = Animator.StringToHash(nameof(MovementSpeed));
+		}
+		
+		[field: SerializeField]
+		public Animator Animator { get; set; }
+
+		[field: SerializeField]
+		public UnitPresenter UnitPresenter { get; set; }
+		
+		private void Update()
+		{
+			Animator.SetBool(AnimatorVariables.IsMoving, UnitPresenter.NavMeshAgent.velocity.sqrMagnitude > 0);
+			//TODO
+			Animator.SetFloat(AnimatorVariables.MovementSpeed, 1);
+		}
+	}
+}
