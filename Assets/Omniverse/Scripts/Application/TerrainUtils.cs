a188253d95e3725c819b788eb893035c51627458
DavidDreamer <davidb.gamedev@gmail.com>
2024-05-21 22:13:07 +0300

    terrain grass layer + some trees added

---
+++ Assets/Omniverse/Scripts/Application/TerrainUtils.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Omniverse
+{
+	public static class TerrainUtils
+	{
+		[MenuItem("Dreambox/Terrain/Export Trees")]
+		public static void ExportTrees()
+		{
+			var terrain = Selection.activeObject as Terrain;
+
+			if (terrain == null)
+			{
+				return;
+			}
+
+			Transform treesHolder = terrain.transform.Find("Trees");
+			if (treesHolder == null)
+			{
+				treesHolder = new GameObject("Trees").transform;
+				treesHolder.SetParent(terrain.transform, false);
+			}
+
+			foreach (var VARIABLE in terrain.terrainData.treeInstances)
+			{
+				
+			}
+		}
+	}
+}
