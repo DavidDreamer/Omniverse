4e012a8874de8255649146fd5cab5c96ef0448ee
DavidDreamer <davidb.gamedev@gmail.com>
2024-05-23 13:51:58 +0300

    fow properties extracted

---
+++ Assets/Omniverse/Scripts/FogOfWar/Rendering/FogOfWar.hlsl
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include <HLSLSupport.cginc>
+
+CBUFFER_START(FogOfWarProperties)
+float4 FogOfWarUnexploredColor;
+float4 FogOfWarConcealedColor;
+CBUFFER_END
+
+uniform sampler2D FogOfWarTexture;
+
+#define CELL_VISIBILITY_UNEXPLORED 0
+#define CELL_VISIBILITY_CONCEALED 1
+#define CELL_VISIBILITY_VISIBLE 2
