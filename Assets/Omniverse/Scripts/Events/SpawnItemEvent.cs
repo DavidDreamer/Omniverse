3f3dd04b06196b859bdbaedb7ffb470a7b40c33d
DavidDreamer <davidb.gamedev@gmail.com>
2024-06-19 01:51:58 +0300

    temp item flower added

---
+++ Assets/Omniverse/Scripts/Events/SpawnItemEvent.cs
@@ -0,0 +1,20 @@
+﻿using Omniverse.Items;
+using UnityEngine;
+using VContainer;
+
+namespace Omniverse.Units
+{
+	public class SpawnItemEvent: MonoBehaviour
+	{
+		[field: SerializeField]
+		private ItemDesc Desc { get; set; }
+
+		[Inject]
+		private Items.Manager ItemsManager { get; set; }
+		
+		public void Start()
+		{
+			ItemsManager.Spawn(Desc, transform.position, transform.rotation);
+		}
+	}
+}
