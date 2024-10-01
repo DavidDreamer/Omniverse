c31a225e354514ff74ed82672ef4dc3d9c61ae27
DavidDreamer <davidb.gamedev@gmail.com>
2024-10-01 13:09:18 +0300

    faction utils improved

--- Assets/Scripts/Factions/FactionUtils.cs
+++ Assets/Scripts/Factions/FactionUtils.cs
@@ -5,5 +5,25 @@
 		public static bool IsAllyFor(this IFactious first, IFactious second) => first.FactionID == second.FactionID;
 
 		public static bool IsEnemyFor(this IFactious first, IFactious second) => first.FactionID != second.FactionID;
+
+		public static bool Match(this FactiousFilter filter, IFactious first, IFactious second)
+		{
+			if (filter.HasFlag(FactiousFilter.Self) && first == second)
+			{
+				return true;
+			}
+
+			if (filter.HasFlag(FactiousFilter.Ally) && first.IsAllyFor(second))
+			{
+				return true;
+			}
+
+			if (filter.HasFlag(FactiousFilter.Enemy) && first.IsEnemyFor(second))
+			{
+				return true;
+			}
+
+			return false;
+		}
 	}
 }
