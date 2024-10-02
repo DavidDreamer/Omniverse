50a2b3060fca4368b7d314e206371cabc86c9008
DavidDreamer <davidb.gamedev@gmail.com>
2024-10-03 00:38:13 +0300

    SwitchTargetFaction operation added

---
+++ Assets/Scripts/Actions/Units/SwitchTargetFaction.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using Cysharp.Threading.Tasks;
+using Omniverse.Units;
+using UnityEngine;
+
+namespace Omniverse.Actions
+{
+	public class SwitchTargetFaction : Operation
+	{
+		[field: SerializeField]
+		[field: OperationPicker]
+		public Operation Self { get; private set; }
+
+		[field: SerializeField]
+		[field: OperationPicker]
+		public Operation Ally { get; private set; }
+
+		[field: SerializeField]
+		[field: OperationPicker]
+		public Operation Enemy { get; private set; }
+
+		public override async UniTask<Operation> PerformAsync(ExecutionContext context, System.Threading.CancellationToken token)
+		{
+			await UniTask.CompletedTask;
+
+			var unit = context.Units().First();
+			var caster = (Unit)context.Caster;
+
+			if (caster == unit)
+			{
+				return Self;
+			}
+			else if (caster.IsAllyFor(unit))
+			{
+				return Ally;
+			}
+			else
+			{
+				return Enemy;
+			}
+		}
+	}
+}
