365fcf9f270e2c2cc19c45138e4f066447685480
DavidDreamer <davidb.gamedev@gmail.com>
2024-10-06 02:18:08 +0300

    test abilities wip

---
+++ Assets/Scripts/Operations/Units/Teleport.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using Omniverse.Units;
+using UnityEngine;
+
+namespace Omniverse.Actions
+{
+	public class Teleport : Action
+	{
+		public override UniTask Perform(OperationContext context, CancellationToken token)
+		{
+			Vector3 position = context.Points.First();
+
+			foreach (Unit unit in context.Units())
+			{
+				unit.NavMeshAgent.Warp(position);
+			}
+
+			return UniTask.CompletedTask;
+		}
+	}
+}
