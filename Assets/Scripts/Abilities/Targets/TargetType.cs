49da90fb1f0fc15cd4b35158e2496f25df332ef8
DavidDreamer <davidb.gamedev@gmail.com>
2024-10-20 03:37:00 +0300

    target ability enum fixed

--- Assets/Scripts/Abilities/Targets/TargetType.cs
+++ Assets/Scripts/Abilities/Targets/TargetType.cs
@@ -1,14 +1,11 @@
-﻿using System;
-
-namespace Omniverse.Abilities
+﻿namespace Omniverse.Abilities
 {
-	[Flags]
 	public enum TargetType
 	{
-		None = 0,
-		Point = 1 << 0,
-		Direction = 1 << 1,
-		Unit = 1 << 2,
-		ResourceSource = 1 << 3
+		None,
+		Point,
+		Direction,
+		Unit,
+		ResourceSource
 	}
 }
