9bd579ed9c0fdb9439564e6f46ecbb340cb00971
DavidDreamer <davidb.gamedev@gmail.com>
2024-10-23 04:12:19 +0300

    effect removed operation fixed

---
+++ Assets/Scripts/Operations/Actions/ModifyProperty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Omniverse
+{
+	[Serializable]
+	public class ModifyProperty : IAction<Unit>
+	{
+		[field: SerializeField]
+		public PropertyID Property { get; private set; }
+
+		[field: SerializeField]
+		public PropertyModifier Modifier { get; private set; }
+
+		public void Perform(Entity actor, Unit target)
+		{
+			target.ModifyProperty(Property, Modifier, actor);
+		}
+	}
+}
