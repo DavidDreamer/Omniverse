a9afb30102ef7b86175c978741be093f292f8473
DavidDreamer <davidb.gamedev@gmail.com>
2025-03-22 16:14:49 +0300

    rendering initialization only fro client

---
+++ Assets/Scripts/Rendering/RenderingClient.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Omniverse.Rendering
+{
+	//TODO ECS
+	public class RenderingClient : MonoBehaviour
+	{
+	}
+}
--- Assets/Scripts/Rendering/RenderingInitializationSystem.cs
+++ Assets/Scripts/Rendering/RenderingInitializationSystem.cs
@@ -1,17 +1,15 @@
-﻿using Omniverse.Mapping;
-using Unity.Entities;
+﻿using Unity.Entities;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Omniverse.Rendering
 {
-	[UpdateInGroup(typeof(SimulationSystemGroup), OrderLast = true)]
+	[UpdateInGroup(typeof(PresentationSystemGroup), OrderLast = true)]
 	public partial struct RenderingInitializationSystem : ISystem, ISystemStartStop
 	{
 		public void OnStartRunning(ref SystemState state)
 		{
-			Object.FindFirstObjectByType<FogOfWarRenderer>(FindObjectsInactive.Include).gameObject.SetActive(true);
-			Object.FindFirstObjectByType<MapRenderer>(FindObjectsInactive.Include).gameObject.SetActive(true);
+			Object.FindFirstObjectByType<RenderingClient>(FindObjectsInactive.Include).gameObject.SetActive(true);
 			Object.FindFirstObjectByType<CanvasScaler>(FindObjectsInactive.Include).gameObject.SetActive(true);
 		}
 
