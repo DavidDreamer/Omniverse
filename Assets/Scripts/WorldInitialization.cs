68b5746a5c14ddc734b59625704a16221c9a7188
DavidDreamer <davidb.gamedev@gmail.com>
2025-03-23 02:04:54 +0300

    automatic world creation disabled

---
+++ Assets/Scripts/WorldInitialization.cs
@@ -0,0 +1,30 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace Omniverse
+{
+	public class WorldInitialization : MonoBehaviour
+	{
+		public int SkipFrames;
+
+		int framesSkipped;
+
+		//public void Update()
+		//{
+		//	if (framesSkipped == SkipFrames)
+		//	{
+		//		DefaultWorldInitialization.Initialize("Default World", false);
+		//		Destroy(gameObject);
+		//		return;
+		//	}
+
+		//	framesSkipped++;
+		//}
+
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+		static void Init()
+		{
+			DefaultWorldInitialization.Initialize("Default World", false);
+		}
+	}
+}
