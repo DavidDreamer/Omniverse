79c4c0e2a42ca71b36c3f315b7e9285d2234d1a5
DavidDreamer <davidb.gamedev@gmail.com>
2025-04-05 14:44:32 +0300

    code cleanup

---
+++ Assets/Scripts/Components/Health.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.NetCode;
+
+namespace Omniverse
+{
+	[BurstCompile]
+	public struct Health : IComponentData
+	{
+		[GhostField]
+		public float Maximum;
+
+		[GhostField]
+		public float Current;
+	}
+}
---
+++ Assets/Scripts/Debug/DecreaseHealthSystem.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+
+namespace Omniverse
+{
+	[WorldSystemFilter(WorldSystemFilterFlags.ServerSimulation)]
+	[DisableAutoCreation]
+	public partial struct DecreaseHealthSystem : ISystem
+	{
+		public void OnUpdate(ref SystemState state)
+		{
+			foreach (var health in SystemAPI.Query<RefRW<Health>>())
+			{
+				health.ValueRW.Current -= 0.01f;
+			}
+		}
+	}
+}
