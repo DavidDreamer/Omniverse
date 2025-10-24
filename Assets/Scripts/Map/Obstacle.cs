40eaa84567c3a37f681035e1ea3e2444ba44de9a
DavidDreamer <davidb.gamedev@gmail.com>
2025-10-25 02:46:05 +0300

    missed component added

---
+++ Assets/Scripts/Map/Obstacle.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Omniverse
+{
+	[BurstCompile]
+	public struct Obstacle : IComponentData
+	{
+		public int2 Start;
+		public int2 End;
+	}
+}
